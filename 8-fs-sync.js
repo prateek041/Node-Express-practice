fa7d99d041bfe2ef422142d32be322b4c9895267
prateek041 <prateeksingh9741@gmail.com>
2022-06-19 07:36:42 +0530

    async js functions

--- 8-fs-sync.js
+++ 8-fs-sync.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFile, writeFile } = require('fs');
 
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFile, writeFile } = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf-8')
-const second = readFileSync('./content/second.txt', 'utf-8')
-
-// console.log(first, second)
-
-writeFileSync('./content/result.txt', `This is a mixutre of ${first}. ${second}`, { flag: 'a' })
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return;
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf-8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        const second = result;
+        writeFile('./content/result-async.txt', `Here is the result file: ${first} ${second}`, (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            console.log(result)
+        })
+    })
+})
