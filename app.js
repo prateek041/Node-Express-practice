80c4ef8d7dd35e6bc6a0609463f3f4774d31192c
prateek041 <prateeksingh9741@gmail.com>
2022-06-28 18:54:16 +0530

    promisify read write

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-const { readFile } = require('fs')
-const { get } = require('lodash')
+const { readFile, writeFile } = require('fs')
 
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
+const util = require('util')
+
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
 
 const start = async () => {
     try {
-        const first = await getText("./content/first.txt")
-        const second = await getText("./content/second.txt")
-        console.log(first);
+        const first = await readFilePromise("./content/first.txt", "utf-8")
+        const second = await writeFilePromise("./content/second.txt", "utf-8")
+        await writeFilePromise("./content/promise-write-file.txt", `Here is the result file: ${first} ${second}`)
     }
     catch (error) {
         console.log(error);
@@ -25,6 +18,18 @@ const start = async () => {
 }
 
 start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 
 // const start = async () => {
 //     try {
