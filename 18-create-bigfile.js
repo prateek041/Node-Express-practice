74cfbb6c34c89532c3660725dc811e7b59d24313
prateek041 <prateeksingh9741@gmail.com>
2022-06-29 17:07:18 +0530

    stream chunk

--- 18-create-bigfile.js
+++ 18-create-bigfile.js
@@ -1,5 +1,5 @@
 const { writeFileSync } = require('fs')
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 100000; i++) {
     writeFileSync('./content/big.txt', `hello, this is prateek ${i}\n`, { flag: 'a' })
 }
