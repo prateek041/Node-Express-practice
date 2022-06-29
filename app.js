7bcf28dc424cbf55a98f89762afe921888a719b2
prateek041 <prateeksingh9741@gmail.com>
2022-06-29 17:22:00 +0530

    stream chunked

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require('http')
 const fs = require('fs')
+const { fileURLToPath } = require('url')
 
 http.createServer((req, res) => {
-    const text = fs.readFileSync('./content/big.txt', 'utf-8')
-    res.end(text)
+    // const text = fs.readFileSync('./content/big.txt', 'utf-8')
+    // res.end(text)
+    const readStream = fs.createReadStream('./content/big.txt', 'utf-8')
+    readStream.on('open', () => {
+        readStream.pipe(res)
+    })
+    readStream.on('err', () => {
+        res.end("error")
+    })
 })
     .listen(5001)
